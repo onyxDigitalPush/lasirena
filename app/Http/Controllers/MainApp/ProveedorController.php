06994dd4c7f5487b30a424bf41d7b6e4e7f8a2cc
david-fugs <fulgore0142@gmail.com>
2025-05-23 13:33:45 -0500

    cambios

---
+++ app/Http/Controllers/MainApp/ProveedorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\MainApp;
+
+use App\Models\MainApp\Proveedor;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProveedorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MainApp\Proveedor  $proveedor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Proveedor $proveedor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MainApp\Proveedor  $proveedor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Proveedor $proveedor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MainApp\Proveedor  $proveedor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Proveedor $proveedor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MainApp\Proveedor  $proveedor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Proveedor $proveedor)
+    {
+        //
+    }
+}
